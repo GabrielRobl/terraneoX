493611c8d7ca62f870ddfc35690f8a7913763d8c
Gabriel Robl <grobl@buch.geophysik.uni-muenchen.de>
2026-07-09 13:11:09 +0200

    Inconsistency fix for redimensionalisation of max radial grid distance in logging.

--- apps/mantlecirculation/src/energy_solver.hpp
+++ apps/mantlecirculation/src/energy_solver.hpp
@@ -221,7 +221,8 @@ class SUPGSolver : public EnergySolver< ScalarType >
         util::logroot << "Computing dt (SUPG advection CFL) ..." << std::endl;
         util::logroot << "    max_vel (cm/a) :             " << max_vel * prm_.physics_parameters.calc_cm_per_year
                       << std::endl;
-        util::logroot << "    h (m) :                      " << h_ * prm_.mesh_parameters.radius_surface_m << std::endl;
+        util::logroot << "    h (m) :                      " << h_ * prm_.mesh_parameters.mantle_thickness_m
+                      << std::endl;
         util::logroot << "    cfl timestep size (= dt_scaling * h/v_max): "
                       << dt_cfl * prm_.physics_parameters.calc_time_Ma << " Ma" << std::endl;
         if ( dt_cfl > prm_.time_stepping_parameters.dt_max )
@@ -533,7 +534,8 @@ class EVSolver : public EnergySolver< ScalarType >
         util::logroot << "Computing dt (EV advection CFL) ..." << std::endl;
         util::logroot << "    max_vel (cm/a) :             " << max_vel * prm_.physics_parameters.calc_cm_per_year
                       << std::endl;
-        util::logroot << "    h (m) :                      " << h_ * prm_.mesh_parameters.radius_surface_m << std::endl;
+        util::logroot << "    h (m) :                      " << h_ * prm_.mesh_parameters.mantle_thickness_m
+                      << std::endl;
         util::logroot << "    cfl timestep size (= dt_scaling * h/v_max): "
                       << dt_cfl * prm_.physics_parameters.calc_time_Ma << " Ma " << std::endl;
         if ( dt_cfl > prm_.time_stepping_parameters.dt_max )
